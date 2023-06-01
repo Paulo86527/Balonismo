6fb4b68ebb0e2db35a27f3bdfa7453ea357d33fa
PH86527 <clctrovao2020@gmail.com>
2023-06-01 08:20:27 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+         Casal pessoa1;
+         Casal pessoa2;
+
+         pessoa1 = new Casal();
+         pessoa2 = new Casal();
+
+    }
+
+
+
+}
