c07d0016b914c89607e0a4bbaa23285effdb6654
PH86527 <clctrovao2020@gmail.com>
2023-06-06 08:29:40 -0300

    detalhes das funcionalidades das entidades

--- src/Balao.java
+++ src/Balao.java
@@ -1,5 +1,17 @@
 public class Balao {
 
+    int id;
+    String registro;
+    String cor;
+    String tipo;
+    int capacidade;
+    double tamanho;
+    double peso;
+    String porte;
+
+    public String voar(){
+        return"voando";
+    }
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
 
-
+           Balao b1 = new Balao();
+           String resultado = b1.voar();
+        System.out.println(resultado);
     }
 
 
