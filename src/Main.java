9861801790870c77218167f42d69ab3e33da0d56
PH86527 <clctrovao2020@gmail.com>
2023-06-06 08:13:02 -0300

    primeira calsse

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-         Casal pessoa1;
-         Casal pessoa2;
 
-         pessoa1 = new Casal();
-         pessoa2 = new Casal();
 
     }
 
---
+++ src/Pessoa.java
@@ -0,0 +1,15 @@
+public class Pessoa
+{
+
+    String nome = "Casal";
+    
+    int cpf = 0;
+
+    int rg = 0;
+
+    int telefone = 0;
+
+    int tel_Contato = 0;
+
+    String endereço = "endereço";
+}
